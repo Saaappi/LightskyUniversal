81897ea64036274153c6945e5a1e01deaf136ec7
Saaappi <alex.metz1024@gmail.com>
2025-05-03 15:16:04 -0700

    Cinematic cancellation can now be blocked by holding the shift key down before the cinematic plays.

--- Modules/Cinematics.lua
+++ Modules/Cinematics.lua
@@ -12,13 +12,13 @@ local function IsCutsceneProtected()
 end
 
 CinematicFrame:HookScript("OnShow", function(self, ...)
-	if not IsCutsceneProtected() then
+	if not IsCutsceneProtected() and not IsShiftKeyDown() then
 		CinematicFrame_CancelCinematic()
 	end
 end)
 
 hooksecurefunc("MovieFrame_PlayMovie", function(self, movieID)
-	if not IsCutsceneProtected() then
+	if not IsCutsceneProtected() and not IsShiftKeyDown() then
 		MovieFrame:Hide()
 	end
 end)
