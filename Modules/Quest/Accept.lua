d61fdb4f1114e270b8be90ecad114d39a00153bb
Saaappi <alex.metz1024@gmail.com>
2025-05-13 20:18:42 -0700

    Repeatable quests should no longer be accepted.

--- Modules/Quest/Accept.lua
+++ Modules/Quest/Accept.lua
@@ -7,7 +7,7 @@ eventFrame:SetScript("OnEvent", function(_, event, ...)
     if event == "QUEST_DETAIL" then
         C_Timer.After(0.2, function()
             local questID = GetQuestID()
-            if questID and not LSU.IsQuestIgnored(questID) then
+            if questID and (not LSU.IsQuestIgnored(questID) and not C_QuestLog.IsRepeatableQuest(questID) and not QuestIsWeekly()) then
                 AcceptQuest()
             end
         end)
@@ -27,7 +27,7 @@ eventFrame:SetScript("OnEvent", function(_, event, ...)
             for i = 1, GetNumAvailableQuests() do
                 local questID = select(5, GetAvailableQuestInfo(i))
                 if questID and not LSU.IsQuestIgnored(questID) then
-                    if not QuestIsDaily() and not QuestIsWeekly() and not C_QuestLog.IsRepeatableQuest(questID) then
+                    if not QuestIsDaily() and (not QuestIsWeekly() and not C_QuestLog.IsRepeatableQuest(questID)) then
                         SelectAvailableQuest(i)
                     end
                 end
