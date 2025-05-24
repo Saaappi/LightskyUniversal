d645fa28ee546cb8a6f55d65ee116af075bf547b
Saaappi <alex.metz1024@gmail.com>
2025-05-23 20:53:28 -0700

    Removed the Data\Properties file. Moved Enum.Junk to its own file.

---
+++ Data/Junk.lua
@@ -0,0 +1,25 @@
+local LSU = select(2, ...)
+
+LSU.Enum.Junk = {
+    13444,  -- Major Mana Potion
+    13446,  -- Major Healing Potion
+    22829,  -- Super Healing Potion
+    22832,  -- Super Mana Potion
+    27854,  -- Smoked Talbuk Venison
+    27855,  -- Mag'har Grainbread
+    27860,  -- Purified Draenic Water
+    28399,  -- Filtered Draenic Water
+    29449,  -- Bladespire Bagel
+    29451,  -- Clefthoof Ribs
+    30810,  -- Sunfury Signet
+    32902,  -- Bottled Nethergon Energy
+    32905,  -- Bottled Nethergon Vapor
+    33447,  -- Runic Healing Potion
+    33457,  -- Scroll of Agility VI
+    33459,  -- Scroll of Protection VI
+    37091,  -- Scroll of Intellect VII
+    37093,  -- Scroll of Stamina VII
+    43463,  -- Scroll of Agility VII
+    43465,  -- Scroll of Strength VII
+    43467,  -- Scroll of Protection VII
+}
--- Data/Properties.lua
+++
@@ -1,9 +0,0 @@
-local addonName, LSU = ...
-
-local Properties = {
-    ["About"] = {
-        ["Version"] = C_AddOns.GetAddOnMetadata(addonName, "Version"),
-        ["Author"] = C_AddOns.GetAddOnMetadata(addonName, "Author")
-    }
-}
-LSU.Properties = Properties
