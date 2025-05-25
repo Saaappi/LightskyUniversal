2e21d55c804102731e2e995a3809f369b6eaca22
Saaappi <alex.metz1024@gmail.com>
2025-05-25 15:14:36 -0700

    2505.13: Moved player talents to their own file and adjusted the OnClick handler for the talent button.

--- Buttons/TalentButton.lua
+++ Buttons/TalentButton.lua
@@ -1,11 +1,6 @@
 local addonName, LSU = ...
 local applyTalentsButton
 
-local talents = {
-    lastUpdated="2025-05-14 22:19:18",
-    [1]={specs={[72]="CgEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0YgZZmZYsMmZmFmZmxMYYmttZGzM2GmxYGzMjFbzwMjBAAACDsBLLGNmBwsAjF"}},[2]={specs={[66]="CIEAAAAAAAAAAAAAAAAAAAAAAsNzMDz2YhZMzM2mZmZmxYYAAAGAAAAAAASmZYYmhxMmt2AgxAGAD2GAAgZm2mlZbGAgNDMAMzwwYA"}},[3]={specs={[254]="C4PAAAAAAAAAAAAAAAAAAAAAAwADsMGNWGAzG2AAAAAAAAAAAAAAYGzYGzMDzoZGjhZGGDzsMG2GjZxsMMjZmZWmZGGzMz2yYw2A"}},[4]={specs={[261]="CUQAAAAAAAAAAAAAAAAAAAAAAAAM2mBAAAAAgZbMWmGzYmxMGjZYmZmhxsNLGjttZmxMzMGMWmlBAAAgZwAYMbGGYgZhFaxGM"}},[5]={specs={[258]="CIQAAAAAAAAAAAAAAAAAAAAAAMgZGAAAAAAAAAAAAMmZxMmhtZbmZmZmtxMLDmNmZmZMbMLYwMmFz2UzMAMAmZzywsZAIjxCAA"}},[6]={specs={[252]="CwPAAAAAAAAAAAAAAAAAAAAAAAgZGzMjZYYGmZmpZMjZGzYAAAAAAAAAMMzYGjZmZAAbgZxYmZmxMzA2MLGGYgZhhGLYAmBA"}},[7]={specs={[263]="CcQAAAAAAAAAAAAAAAAAAAAAAMzMzCYmx2MzMzMzyMjtxAAAAAAAAAAAwiZMsBbwMM0gFAMLTmBzyiZmZYmZmZBLmZCWGLjZGAgBA"}},[8]={specs={[62]="C4DAAAAAAAAAAAAAAAAAAAAAAMjhxsgZmZMLmBjZMmmHYMDAAAAAADAYmZaZ2WmBAwGAAAAAALAsMGmZWmlhZwMjZmZmZmxMGD"}},[9]={specs={[267]="CsQAAAAAAAAAAAAAAAAAAAAAAAmZmZmZEzihxsZmZYWmNjZmxsY2YbxMDAAAAMzMsNzsMjFYgZxoxMAmNshBAAAAAAwYmxsAA"}},[10]={specs={[268]="CwQAAAAAAAAAAAAAAAAAAAAAAAAAAAzMGzgxyMz2YmZAAAAAAAYZBEzMwMMYGsMzMDzyYmx2sMTLbWsNmxMbAAwGAAAwsNbNzMzCzwG"}},[11]={specs={[104]="CgGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAzYMzMzyMMmZMGLMMzsYZALGGNRzMzyMbzMzyYGAAAAAAwwMDYZbmNMmlBwEAAAwGMAsZmhB"}},[12]={specs={[581]="CUkAAAAAAAAAAAAAAAAAAAAAAAAGjZmZMmZkZmxMDzMLjBjtZMjZMzwY2mZmtZmhZwAAAAAsMLmxwsx0AzMwG"}},[13]={specs={[1467]="CsbBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAmZmBzsZGmBMYMTzYmJz2YZYmZmZMz2MgZMzGzMbzMDMwYwCsMGN2GAzAwGG"}}
-}
-
 EventRegistry:RegisterCallback("PlayerSpellsFrame.TalentTab.Show", function()
     local parentFrame = PlayerSpellsFrame
     local talentsFrame = parentFrame.TalentsTab or PlayerSpellsFrame.TalentsFrame
@@ -241,14 +236,10 @@ EventRegistry:RegisterCallback("PlayerSpellsFrame.TalentTab.Show", function()
     end
 
     applyTalentsButton:SetScript("OnClick", function()
-        C_Timer.After(0, function()
-            if talents[LSU.Character.ClassID] then
-                local specs = talents[LSU.Character.ClassID].specs
-                if specs[LSU.Character.SpecID] then
-                    ImportText(specs[LSU.Character.SpecID])
-                end
-            end
-        end)
+        local talents = LSU.Enum.PlayerTalents[LSU.Character.ClassID]["specs"][LSU.Character.SpecID]
+        if talents then
+            ImportText(talents)
+        end
     end)
 end)
 
---
+++ Data/Talents.lua
@@ -0,0 +1,70 @@
+local LSU = select(2, ...)
+
+LSU.Enum.PlayerTalents = {
+    LastUpdated = "2025-05-14 22:19:18",
+    [1] = {
+        specs = {
+            [72] = "CgEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0YgZZmZYsMmZmFmZmxMYYmttZGzM2GmxYGzMjFbzwMjBAAACDsBLLGNmBwsAjF"
+        }
+    },
+    [2] = {
+        specs = {
+            [66] = "CIEAAAAAAAAAAAAAAAAAAAAAAsNzMDz2YhZMzM2mZmZmxYYAAAGAAAAAAASmZYYmhxMmt2AgxAGAD2GAAgZm2mlZbGAgNDMAMzwwYA"
+        }
+    },
+    [3] = {
+        specs = {
+            [254] = "C4PAAAAAAAAAAAAAAAAAAAAAAwADsMGNWGAzG2AAAAAAAAAAAAAAYGzYGzMDzoZGjhZGGDzsMG2GjZxsMMjZmZWmZGGzMz2yYw2A"
+        }
+    },
+    [4] = {
+        specs = {
+            [261] = "CUQAAAAAAAAAAAAAAAAAAAAAAAAM2mBAAAAAgZbMWmGzYmxMGjZYmZmhxsNLGjttZmxMzMGMWmlBAAAgZwAYMbGGYgZhFaxGM"
+        }
+    },
+    [5] = {
+        specs = {
+            [258] = "CIQAAAAAAAAAAAAAAAAAAAAAAMgZGAAAAAAAAAAAAMmZxMmhtZbmZmZmtxMLDmNmZmZMbMLYwMmFz2UzMAMAmZzywsZAIjxCAA"
+        }
+    },
+    [6] = {
+        specs = {
+            [252] = "CwPAAAAAAAAAAAAAAAAAAAAAAAgZGzMjZYYGmZmpZMjZGzYAAAAAAAAAMMzYGjZmZAAbgZxYmZmxMzA2MLGGYgZhhGLYAmBA"
+        }
+    },
+    [7] = {
+        specs = {
+            [263] = "CcQAAAAAAAAAAAAAAAAAAAAAAMzMzCYmx2MzMzMzyMjtxAAAAAAAAAAAwiZMsBbwMM0gFAMLTmBzyiZmZYmZmZBLmZCWGLjZGAgBA"
+        }
+    },
+    [8] = {
+        specs = {
+            [62] = "C4DAAAAAAAAAAAAAAAAAAAAAAMjhxsgZmZMLmBjZMmmHYMDAAAAAADAYmZaZ2WmBAwGAAAAAALAsMGmZWmlhZwMjZmZmZmxMGD"
+        }
+    },
+    [9] = {
+        specs = {
+            [267] = "CsQAAAAAAAAAAAAAAAAAAAAAAAmZmZmZEzihxsZmZYWmNjZmxsY2YbxMDAAAAMzMsNzsMjFYgZxoxMAmNshBAAAAAAwYmxsAA"
+        }
+    },
+    [10] = {
+        specs = {
+            [268] = "CwQAAAAAAAAAAAAAAAAAAAAAAAAAAAzMGzgxyMz2YmZAAAAAAAYZBEzMwMMYGsMzMDzyYmx2sMTLbWsNmxMbAAwGAAAwsNbNzMzCzwG"
+        }
+    },
+    [11] = {
+        specs = {
+            [104] = "CgGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAzYMzMzyMMmZMGLMMzsYZALGGNRzMzyMbzMzyYGAAAAAAwwMDYZbmNMmlBwEAAAwGMAsZmhB"
+        }
+    },
+    [12] = {
+        specs = {
+            [581] = "CUkAAAAAAAAAAAAAAAAAAAAAAAAGjZmZMmZkZmxMDzMLjBjtZMjZMzwY2mZmtZmhZwAAAAAsMLmxwsx0AzMwG"
+        }
+    },
+    [13] = {
+        specs = {
+            [1467] = "CsbBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAmZmBzsZGmBMYMTzYmJz2YZYmZmZMz2MgZMzGzMbzMDMwYwCsMGN2GAzAwGG"
+        }
+    }
+}
