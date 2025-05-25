74b7e791ffa88cf4de18926cb3bdaec095f1fe7e
Saaappi <alex.metz1024@gmail.com>
2025-05-25 16:43:17 -0700

    2505.16: Efficiency improvements to the chat icons module. Pixel perfection for the item texture and collection status icons.

--- Modules/ChatIcons.lua
+++ Modules/ChatIcons.lua
@@ -1,53 +1,75 @@
 local addonName = ...
 local iconPath = string.format("Interface\\AddOns\\%s\\Data\\Icons", addonName)
+local iconSize, yOffset = 14, -2
+local excludedEquipLocs = {
+    INVTYPE_FINGER = true,
+    INVTYPE_TRINKET = true,
+    INVTYPE_NECK = true
+}
+
+--[[ TODO
+    - Cache items and use lookups to show icons 
+]]
+
+local function IconTag(path)
+    return string.format("|T%s:%d:%d:0:%d|t", path, iconSize, iconSize, yOffset)
+end
+
+local function FormatWithIcons(texture, itemLink, collectedTexture)
+    return string.format("%s %s %s", IconTag(texture), itemLink, IconTag(collectedTexture))
+end
+
+local function GetCollectedStatusIcon(isCollected, path)
+    if isCollected then
+        return string.format("%s\\KNOWN", path)
+    end
+    return string.format("%s\\UNKNOWN", path)
+end
+
+local function GetAppearanceAndCollectedInfoByItemLink(itemLink)
+    local sourceID = select(2, C_TransmogCollection.GetItemInfo(itemLink))
+    if not sourceID then return false end
+
+    local isCollected = select(5, C_TransmogCollection.GetAppearanceSourceInfo(sourceID))
+    if not isCollected then return false end
+
+    return isCollected
+end
+
+local function GetItemIcon(itemLink)
+    local texture = C_Item.GetItemIconByID(itemLink)
+    local itemType, _, _, equipLoc = select(6, C_Item.GetItemInfo(itemLink))
+    if texture
+        and (itemType == "Armor" or itemType == "Weapon")
+        and not excludedEquipLocs[equipLoc]
+    then
+        local isCollected = GetAppearanceAndCollectedInfoByItemLink(itemLink)
+        if isCollected ~= nil then
+            local collectedTexture = GetCollectedStatusIcon(isCollected, iconPath)
+            local formattedString = FormatWithIcons(texture, itemLink, collectedTexture)
+            return formattedString
+        end
+    end
+    return IconTag(texture) .. " " .. itemLink
+end
+
+local function GetAchievementIcon(achievementLink)
+    local achievementID = string.match(achievementLink, "|Hachievement:(%d+):"); achievementID = tonumber(achievementID)
+    local icon = select(10, GetAchievementInfo(achievementID))
+    if icon then
+        return IconTag(icon) .. " " .. achievementLink
+    end
+end
+
+local function GetSpellIcon(spellLink)
+    local spellID = string.match(spellLink, "|Hspell:(%d+):"); spellID = tonumber(spellID)
+    local icon = (C_Spell.GetSpellInfo(spellID)).iconID
+    if icon then
+        return IconTag(icon) .. " " .. spellLink
+    end
+end
 
 local function Main(_, _, msg, ...)
-    local function GetCollectedStatusIcon(isCollected, path)
-        if isCollected then
-            return string.format("%s\\KNOWN", path)
-        end
-        return string.format("%s\\UNKNOWN", path)
-    end
-
-    local function GetAppearanceAndCollectedInfoByItemLink(itemLink)
-        local sourceID = select(2, C_TransmogCollection.GetItemInfo(itemLink))
-        if not sourceID then return false end
-
-        local isCollected = select(5, C_TransmogCollection.GetAppearanceSourceInfo(sourceID))
-        if not isCollected then return false end
-
-        return isCollected
-    end
-
-    local function GetItemIcon(itemLink)
-        local texture = C_Item.GetItemIconByID(itemLink)
-        local itemType, _, _, equipLoc = select(6, C_Item.GetItemInfo(itemLink))
-        if texture and (itemType == "Armor" or itemType == "Weapon") and (equipLoc ~= "INVTYPE_FINGER" and equipLoc ~= "INVTYPE_NECK" and equipLoc ~= "INVTYPE_TRINKET") then
-            local isCollected = GetAppearanceAndCollectedInfoByItemLink(itemLink)
-            if isCollected ~= nil then
-                local collectedTexture = GetCollectedStatusIcon(isCollected, iconPath)
-                return string.format("\124T%s:12\124t %s \124T%s:12\124t", texture, itemLink, collectedTexture)
-            end
-        end
-        return string.format("\124T%s:12\124t %s", texture, itemLink)
-    end
-
-    local function GetAchievementIcon(achievementLink)
-        local achievementID = string.match(achievementLink, "|Hachievement:(%d+):"); achievementID = tonumber(achievementID)
-        local icon = select(10, GetAchievementInfo(achievementID))
-        if icon then
-            return string.format("\124T%s:12\124t %s", icon, achievementLink)
-        end
-    end
-
-    local function GetSpellIcon(spellLink)
-        local spellID = string.match(spellLink, "|Hspell:(%d+):"); spellID = tonumber(spellID)
-        local icon = (C_Spell.GetSpellInfo(spellID)).iconID
-        if icon then
-            return string.format("\124T%s:12\124t %s", icon, spellLink)
-        end
-    end
-
     if string.find(msg, "achievement") then
         msg = msg:gsub("(|H([^:|]+):([^|]-)|h(.-)|h)", GetAchievementIcon)
     elseif string.find(msg, "item") then
