089c989a437a6027ac0d86d2c16502d25dfb4d41
Saaappi <alex.metz1024@gmail.com>
2025-05-27 21:03:25 -0700

    2505.21: Implementing the ground work for the Talent Importer utility. Also, added a new frame factory file to support the creation of frames (currently only portrait frames).

---
+++ FrameFactory.lua
@@ -0,0 +1,31 @@
+local LSU = select(2, ...)
+
+LSU.CreateFrame = function(frameType, frameData)
+    assert(type(frameData) == "table", "frameData must be a table!")
+    assert(frameType == "Portrait", "Currently only 'Portrait' frames are supported.")
+
+    -- Set some default values for safety
+    local name = frameData.name or ("LSUFrameAnon" .. math.random(100000))
+    local parent = frameData.parent or UIParent
+    local width = frameData.width or 200
+    local height = frameData.height or 200
+
+    local frame = CreateFrame("Frame", name, parent, "PortraitFrameTemplate")
+    frame:SetSize(width, height)
+    frame:SetClampedToScreen(true)
+    frame:Hide() -- Hide until explicitly shown
+
+    if frameData.movable then
+        frame:SetMovable(true)
+        frame:EnableMouse(true)
+        frame:RegisterForDrag("LeftButton")
+        frame:SetScript("OnDragStart", function(self)
+            self:StartMoving()
+        end)
+        frame:SetScript("OnDragStop", function(self)
+            self:StopMovingOrSizing()
+        end)
+    end
+
+    return frame
+end
