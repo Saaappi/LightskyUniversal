7ec6eb65208fecddec95771dd9c2e3455659d87d
Saaappi <alex.metz1024@gmail.com>
2025-05-27 23:18:24 -0700

    Moved all talent importer data to its data file.

--- Data/Talents.lua
+++ Data/Talents.lua
@@ -1,70 +1,281 @@
 local LSU = select(2, ...)
 
-LSU.Enum.PlayerTalents = {
-    LastUpdated = "2025-05-14 22:19:18",
-    [1] = {
-        specs = {
-            [72] = "CgEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0YgZZmZYsMmZmFmZmxMYYmttZGzM2GmxYGzMjFbzwMjBAAACDsBLLGNmBwsAjF"
+LSU.Enum.PlayerTalents = {}
+LSU.Enum.PlayerTalents.ClassButtons = {
+    {
+        ["id"] = 1,
+        ["atlas"] = "classicon-deathknight",
+        ["classID"] = 6,
+        ["className"] = "Death Knight",
+        ["classColor"] = C_ClassColor.GetClassColor("DEATHKNIGHT"),
+    },
+    {
+        ["id"] = 2,
+        ["atlas"] = "classicon-demonhunter",
+        ["classID"] = 12,
+        ["className"] = "Demon Hunter",
+        ["classColor"] = C_ClassColor.GetClassColor("DEMONHUNTER"),
+    },
+    {
+        ["id"] = 3,
+        ["atlas"] = "classicon-druid",
+        ["classID"] = 11,
+        ["className"] = "Druid",
+        ["classColor"] = C_ClassColor.GetClassColor("DRUID"),
+    },
+    {
+        ["id"] = 4,
+        ["atlas"] = "classicon-evoker",
+        ["classID"] = 13,
+        ["className"] = "Evoker",
+        ["classColor"] = C_ClassColor.GetClassColor("EVOKER"),
+    },
+    {
+        ["id"] = 5,
+        ["atlas"] = "classicon-hunter",
+        ["classID"] = 3,
+        ["className"] = "Hunter",
+        ["classColor"] = C_ClassColor.GetClassColor("HUNTER"),
+    },
+    {
+        ["id"] = 6,
+        ["atlas"] = "classicon-mage",
+        ["classID"] = 8,
+        ["className"] = "Mage",
+        ["classColor"] = C_ClassColor.GetClassColor("MAGE"),
+    },
+    {
+        ["id"] = 7,
+        ["atlas"] = "classicon-monk",
+        ["classID"] = 10,
+        ["className"] = "Monk",
+        ["classColor"] = C_ClassColor.GetClassColor("MONK"),
+    },
+    {
+        ["id"] = 8,
+        ["atlas"] = "classicon-paladin",
+        ["classID"] = 2,
+        ["className"] = "Paladin",
+        ["classColor"] = C_ClassColor.GetClassColor("PALADIN"),
+    },
+    {
+        ["id"] = 9,
+        ["atlas"] = "classicon-priest",
+        ["classID"] = 5,
+        ["className"] = "Priest",
+        ["classColor"] = C_ClassColor.GetClassColor("PRIEST"),
+    },
+    {
+        ["id"] = 10,
+        ["atlas"] = "classicon-rogue",
+        ["classID"] = 4,
+        ["className"] = "Rogue",
+        ["classColor"] = C_ClassColor.GetClassColor("ROGUE"),
+    },
+    {
+        ["id"] = 11,
+        ["atlas"] = "classicon-shaman",
+        ["classID"] = 7,
+        ["className"] = "Shaman",
+        ["classColor"] = C_ClassColor.GetClassColor("SHAMAN"),
+    },
+    {
+        ["id"] = 12,
+        ["atlas"] = "classicon-warlock",
+        ["classID"] = 9,
+        ["className"] = "Warlock",
+        ["classColor"] = C_ClassColor.GetClassColor("WARLOCK"),
+    },
+    {
+        ["id"] = 13,
+        ["atlas"] = "classicon-warrior",
+        ["classID"] = 1,
+        ["className"] = "Warrior",
+        ["classColor"] = C_ClassColor.GetClassColor("WARRIOR"),
+    },
+}
+
+LSU.Enum.PlayerTalents.SpecEditBoxes = {
+    { -- Death Knight
+        {
+            ["id"] = 250,
+            ["name"] = "Blood",
+        },
+        {
+            ["id"] = 251,
+            ["name"] = "Frost",
+        },
+        {
+            ["id"] = 252,
+            ["name"] = "Unholy",
         }
     },
-    [2] = {
-        specs = {
-            [66] = "CIEAAAAAAAAAAAAAAAAAAAAAAsNzMDz2YhZMzM2mZmZmxYYAAAGAAAAAAASmZYYmhxMmt2AgxAGAD2GAAgZm2mlZbGAgNDMAMzwwYA"
+    { -- Demon Hunter
+        {
+            ["id"] = 577,
+            ["name"] = "Havoc",
+        },
+        {
+            ["id"] = 581,
+            ["name"] = "Vengeance",
         }
     },
-    [3] = {
-        specs = {
-            [254] = "C4PAAAAAAAAAAAAAAAAAAAAAAwADsMGNWGAzG2AAAAAAAAAAAAAAYGzYGzMDzoZGjhZGGDzsMG2GjZxsMMjZmZWmZGGzMz2yYw2A"
-        }
+    { -- Druid
+        {
+            ["id"] = 102,
+            ["name"] = "Balance",
+        },
+        {
+            ["id"] = 103,
+            ["name"] = "Feral",
+        },
+        {
+            ["id"] = 104,
+            ["name"] = "Guardian",
+        },
+        {
+            ["id"] = 105,
+            ["name"] = "Restoration",
+        },
     },
-    [4] = {
-        specs = {
-            [261] = "CUQAAAAAAAAAAAAAAAAAAAAAAAAM2mBAAAAAgZbMWmGzYmxMGjZYmZmhxsNLGjttZmxMzMGMWmlBAAAgZwAYMbGGYgZhFaxGM"
-        }
+    { -- Evoker
+        {
+            ["id"] = 1473,
+            ["name"] = "Augmentation",
+        },
+        {
+            ["id"] = 1467,
+            ["name"] = "Devastation",
+        },
+        {
+            ["id"] = 1468,
+            ["name"] = "Preservation",
+        },
     },
-    [5] = {
-        specs = {
-            [258] = "CIQAAAAAAAAAAAAAAAAAAAAAAMgZGAAAAAAAAAAAAMmZxMmhtZbmZmZmtxMLDmNmZmZMbMLYwMmFz2UzMAMAmZzywsZAIjxCAA"
-        }
+    { -- Hunter
+        {
+            ["id"] = 253,
+            ["name"] = "Beast Mastery",
+        },
+        {
+            ["id"] = 254,
+            ["name"] = "Marksmanship",
+        },
+        {
+            ["id"] = 255,
+            ["name"] = "Survival",
+        },
     },
-    [6] = {
-        specs = {
-            [252] = "CwPAAAAAAAAAAAAAAAAAAAAAAAgZGzMjZYYGmZmpZMjZGzYAAAAAAAAAMMzYGjZmZAAbgZxYmZmxMzA2MLGGYgZhhGLYAmBA"
-        }
+    { -- Mage
+        {
+            ["id"] = 62,
+            ["name"] = "Arcane",
+        },
+        {
+            ["id"] = 63,
+            ["name"] = "Fire",
+        },
+        {
+            ["id"] = 64,
+            ["name"] = "Frost",
+        },
     },
-    [7] = {
-        specs = {
-            [263] = "CcQAAAAAAAAAAAAAAAAAAAAAAMzMzCYmx2MzMzMzyMjtxAAAAAAAAAAAwiZMsBbwMM0gFAMLTmBzyiZmZYmZmZBLmZCWGLjZGAgBA"
-        }
+    { -- Monk
+        {
+            ["id"] = 268,
+            ["name"] = "Brewmaster",
+        },
+        {
+            ["id"] = 270,
+            ["name"] = "Mistweaver",
+        },
+        {
+            ["id"] = 269,
+            ["name"] = "Windwalker",
+        },
     },
-    [8] = {
-        specs = {
-            [62] = "C4DAAAAAAAAAAAAAAAAAAAAAAMjhxsgZmZMLmBjZMmmHYMDAAAAAADAYmZaZ2WmBAwGAAAAAALAsMGmZWmlhZwMjZmZmZmxMGD"
-        }
+    { -- Paladin
+        {
+            ["id"] = 65,
+            ["name"] = "Holy",
+        },
+        {
+            ["id"] = 66,
+            ["name"] = "Protection",
+        },
+        {
+            ["id"] = 70,
+            ["name"] = "Retribution",
+        },
     },
-    [9] = {
-        specs = {
-            [267] = "CsQAAAAAAAAAAAAAAAAAAAAAAAmZmZmZEzihxsZmZYWmNjZmxsY2YbxMDAAAAMzMsNzsMjFYgZxoxMAmNshBAAAAAAwYmxsAA"
-        }
+    { -- Priest
+        {
+            ["id"] = 256,
+            ["name"] = "Discipline",
+        },
+        {
+            ["id"] = 257,
+            ["name"] = "Holy",
+        },
+        {
+            ["id"] = 258,
+            ["name"] = "Shadow",
+        },
     },
-    [10] = {
-        specs = {
-            [268] = "CwQAAAAAAAAAAAAAAAAAAAAAAAAAAAzMGzgxyMz2YmZAAAAAAAYZBEzMwMMYGsMzMDzyYmx2sMTLbWsNmxMbAAwGAAAwsNbNzMzCzwG"
-        }
+    { -- Rogue
+        {
+            ["id"] = 259,
+            ["name"] = "Assassination",
+        },
+        {
+            ["id"] = 260,
+            ["name"] = "Outlaw",
+        },
+        {
+            ["id"] = 261,
+            ["name"] = "Subtlety",
+        },
     },
-    [11] = {
-        specs = {
-            [104] = "CgGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAzYMzMzyMMmZMGLMMzsYZALGGNRzMzyMbzMzyYGAAAAAAwwMDYZbmNMmlBwEAAAwGMAsZmhB"
-        }
+    { -- Shaman
+        {
+            ["id"] = 262,
+            ["name"] = "Elemental",
+        },
+        {
+            ["id"] = 263,
+            ["name"] = "Enhancement",
+        },
+        {
+            ["id"] = 264,
+            ["name"] = "Restoration",
+        },
     },
-    [12] = {
-        specs = {
-            [581] = "CUkAAAAAAAAAAAAAAAAAAAAAAAAGjZmZMmZkZmxMDzMLjBjtZMjZMzwY2mZmtZmhZwAAAAAsMLmxwsx0AzMwG"
-        }
+    { -- Warlock
+        {
+            ["id"] = 265,
+            ["name"] = "Affliction",
+        },
+        {
+            ["id"] = 266,
+            ["name"] = "Demonology",
+        },
+        {
+            ["id"] = 267,
+            ["name"] = "Destruction",
+        },
     },
-    [13] = {
-        specs = {
-            [1467] = "CsbBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAmZmBzsZGmBMYMTzYmJz2YZYmZmZMz2MgZMzGzMbzMDMwYwCsMGN2GAzAwGG"
-        }
-    }
+    { -- Warrior
+        {
+            ["id"] = 71,
+            ["name"] = "Arms",
+        },
+        {
+            ["id"] = 72,
+            ["name"] = "Fury",
+        },
+        {
+            ["id"] = 73,
+            ["name"] = "Protection",
+        },
+    },
 }
