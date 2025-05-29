d9ada682eaa958d8d6737e352e2c13b438df4dca
Saaappi <alex.metz1024@gmail.com>
2025-05-28 22:14:03 -0700

    The frame now has sound on open and close and can be closed with the escape key. This logic is now handled in the frame factory. (Talent Importer has been updated to reflect this.)

--- FrameFactory.lua
+++ FrameFactory.lua
@@ -15,6 +15,17 @@ LSU.CreateFrame = function(frameType, frameData)
     frame:SetClampedToScreen(true)
     frame:Hide() -- Hide until explicitly shown
 
+    -- Allow the frame to be closed with the escape key
+    table.insert(UISpecialFrames, frame:GetName())
+
+    -- Give the frame sound when being opened and closed
+    frame:SetScript("OnShow", function()
+        PlaySound(SOUNDKIT.IG_CHARACTER_INFO_OPEN)
+    end)
+    frame:SetScript("OnHide", function()
+        PlaySound(SOUNDKIT.IG_CHARACTER_INFO_CLOSE)
+    end)
+
     if frameData.movable then
         frame:SetMovable(true)
         frame:EnableMouse(true)
