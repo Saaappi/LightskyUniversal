595e21f52fd0f1cdc77809f65bba6d0c84dbb2ca
Saaappi <alex.metz1024@gmail.com>
2025-05-28 18:06:56 -0700

    Renamed the Get Item keybind to Add Junk Item.

--- KeybindHandler.lua
+++ KeybindHandler.lua
@@ -2,12 +2,12 @@ local LSU = select(2, ...)
 
 BINDING_HEADER_LIGHTSKY_UNIVERSAL = "Lightsky Universal"
 BINDING_NAME_LSU_MOUNTUP = "Mount Up"
-BINDING_NAME_LSU_GETITEM = "Get Item"
+BINDING_NAME_LSU_ADDJUNKITEM = "Add Junk Item"
 
 function LSUKeybindHandler(key)
     if key == GetBindingKey("LSU_MOUNTUP") then
         LSU.Mount()
-    elseif key == GetBindingKey("LSU_GETITEM") then
+    elseif key == GetBindingKey("LSU_ADDJUNKITEM") then
         local itemName, itemLink = GameTooltip:GetItem()
         if itemName and itemLink then
             local itemID = C_Item.GetItemInfoInstant(itemLink)
