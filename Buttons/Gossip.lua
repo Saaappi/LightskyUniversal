2cf95bb788e7631bf30df546a5126c816c01b999
Saaappi <alex.metz1024@gmail.com>
2025-05-30 20:42:11 -0700

    The NPC's name and ID are no longer printed when retrieving gossip options.

--- Buttons/Gossip.lua
+++ Buttons/Gossip.lua
@@ -46,16 +46,8 @@ GossipFrame:HookScript("OnShow", function()
     getGossipInfoButton:SetScript("OnClick", function()
         local options = C_GossipInfo.GetOptions()
         if options and #options > 0 then
-            local guid = UnitGUID("npc")
-            local name = UnitName("npc")
-            if guid and name then
-                local id = LSU.Split(guid, "-", 6)
-                print(id .. " - |cff45BA5F" .. name .. "|r")
-                for _, option in ipairs(options) do
-                    print(string.format("|cffBA45A0%s|r - %s", option.gossipOptionID, option.name))
-                end
-            else
-                LSU.PrintWarning(L.TEXT_COULD_NOT_RETRIEVE_NPC_INFO)
+            for _, option in ipairs(options) do
+                print(string.format("|cffBA45A0%s|r - %s", option.gossipOptionID, option.name))
             end
         else
             LSU.PrintWarning(L.TEXT_NO_GOSSIP_OPTIONS_AVAILABLE)
