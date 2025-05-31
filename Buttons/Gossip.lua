591a587a14e85dc113d8e41e28394d163b4b31ee
Saaappi <alex.metz1024@gmail.com>
2025-05-30 23:57:50 -0700

    Removed the button that retrieved gossip options since it's no longer necessary. The button to open the gossip frame has replaced it.

--- Buttons/Gossip.lua
+++ Buttons/Gossip.lua
@@ -1,57 +1,28 @@
 local LSU = select(2, ...)
 local L = LSU.L
 local openGossipFrameButton
-local getGossipInfoButton
 local button = {
     type        = "ActionButton",
     name        = "LSUGetGossipInfoButton",
     parent      = UIParent,
     scale       = 1.05,
-    texture     = 4549145,
+    texture     = 2056011,
     tooltipText = L.GOSSIP_INFO_BUTTON_TOOLTIP
-}
-local openGossipFrameButtonData = {
-    type = "BasicButton",
-    name = "LSUOpenGossipFrameButton",
-    width = 170,
-    height = 25,
-    text = L.LABEL_OPEN_GOSSIPS_BUTTON,
-    point = "CENTER",
-    parent = UIParent
 }
 
 GossipFrame:HookScript("OnShow", function()
-    if not getGossipInfoButton then
-        getGossipInfoButton = LSU.CreateButton(button)
-    end
-
     if not openGossipFrameButton then
-        openGossipFrameButton = LSU.CreateButton(openGossipFrameButtonData)
+        openGossipFrameButton = LSU.CreateButton(button)
     end
-
-    getGossipInfoButton:ClearAllPoints()
-    getGossipInfoButton:SetPoint("TOPLEFT", GossipFrame, "TOPRIGHT", 5, 0)
-    getGossipInfoButton:SetScript("OnClick", function()
-        local options = C_GossipInfo.GetOptions()
-        if options and #options > 0 then
-            for _, option in ipairs(options) do
-                print(string.format("|cffBA45A0%s|r - %s", option.gossipOptionID, option.name))
-            end
-        else
-            LSU.PrintWarning(L.TEXT_NO_GOSSIP_OPTIONS_AVAILABLE)
-        end
-    end)
 
     openGossipFrameButton:ClearAllPoints()
-    openGossipFrameButton:SetPoint("BOTTOM", GossipFrame, "TOP", 0, 5)
+    openGossipFrameButton:SetPoint("TOPLEFT", GossipFrame, "TOPRIGHT", 5, 0)
     openGossipFrameButton:SetScript("OnClick", LSU.OpenGossipFrame)
 
-    getGossipInfoButton:Show()
     openGossipFrameButton:Show()
 end)
 
 GossipFrame:HookScript("OnHide", function()
-    getGossipInfoButton:Hide()
     openGossipFrameButton:Hide()
 end)
 
@@ -74,8 +45,6 @@ hooksecurefunc(GossipFrame, "Update", function(self)
             local newName = string.format("[|cffBA45A0%s|r] ", optionInfo.gossipOptionID) .. optionInfo.name
             if frame.SetTextAndResize then
                 frame:SetTextAndResize(newName)
-            --[[else
-                print(newName)]]
             end
         end
     end
