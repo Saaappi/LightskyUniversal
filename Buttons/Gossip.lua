2bee2e83b0f151bab8e74f6c4a8f7dc862f26fd9
Saaappi <alex.metz1024@gmail.com>
2025-05-31 23:58:46 -0700

    2505.36: Fixed a bug that caused gossip names to overlap on longer strings. This is because the scrollbox parent doesn't auto resize to accommodate the resizing of its children. Not sure if it's a bug, asking more knowledgable people to see what's up.

--- Buttons/Gossip.lua
+++ Buttons/Gossip.lua
@@ -43,11 +43,12 @@ hooksecurefunc(GossipFrame, "Update", function(self)
         for _, frame in self.GreetingPanel.ScrollBox:EnumerateFrames() do
             local frameData = frame:GetData()
             if frameData and frameData.info then
-                newName = string.format("[|cffBA45A0%s|r] ", frameData.info.gossipOptionID) .. frameData.info.name
+                newName = string.format("[|cffBA45A0%s|r] %s", frameData.info.gossipOptionID, frameData.info.name)
             end
             if frame.SetTextAndResize then
                 frame:SetTextAndResize(newName)
             end
         end
+        self.GreetingPanel.ScrollBox:FullUpdateInternal() -- this is necessary because the scrollbox doesn't automatically update when the children are resized
     end
 end)
