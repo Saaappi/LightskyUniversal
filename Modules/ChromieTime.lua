d25a87b66bce43b714555a146b13d15a6b79994d
Saaappi <alex.metz1024@gmail.com>
2025-06-01 00:06:44 -0700

    2506.1: Added a small delay to the Chromie Time (CT) module - gossip is selected too fast and doesn't allow the CT automation to work.

--- Modules/ChromieTime.lua
+++ Modules/ChromieTime.lua
@@ -4,11 +4,14 @@ local eventFrame = CreateFrame("Frame")
 eventFrame:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW")
 eventFrame:SetScript("OnEvent", function(_, event, ...)
     if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
-        local type = ...
-        if type == 45 then
-            if UnitChromieTimeID("player") ~= 10 then
-                C_ChromieTime.SelectChromieTimeOption(10)
+        local args = {...}
+        C_Timer.After(0.15, function(...)
+            local type = args[1]
+            if type == 45 then
+                if UnitChromieTimeID("player") ~= 10 then
+                    C_ChromieTime.SelectChromieTimeOption(10)
+                end
             end
-        end
+        end)
     end
 end)
