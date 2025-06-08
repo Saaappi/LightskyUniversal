e8fb1e121b24072188373b2088f5fa9208747c12
Saaappi <alex.metz1024@gmail.com>
2025-06-07 19:12:15 -0700

    2506.40: Added the remaining settings to the default list.

--- Main.lua
+++ Main.lua
@@ -18,13 +18,17 @@ eventFrame:SetScript("OnEvent", function(_, event, ...)
                     ["AutoTrain.Enabled"] = false,
                     ["BuyQuestItems.Enabled"] = false,
                     ["ChatIcons.Enabled"] = false,
+                    ["ChromieTimeExpansionID"] = 0,
                     ["CompleteQuests.Enabled"] = false,
                     ["Gossip.Enabled"] = false,
+                    ["NewCharacter.Enabled"] = false,
                     ["PlayerTalents.Enabled"] = false,
+                    ["QuestRewards.Enabled"] = false,
                     ["Rares.Enabled"] = false,
                     ["ReadyChecks.Enabled"] = false,
                     ["RoleChecks.Enabled"] = false,
                     ["SkipCinematics.Enabled"] = false,
+                    ["WarbankDepositInCopper"] = 0
                 }
             end
         end
