edc33dcecd8066be345887acf95a4986527e1893
Saaappi <alex.metz1024@gmail.com>
2025-06-07 19:13:22 -0700

    Removed the add junk item keybind.

--- KeybindHandler.lua
+++ KeybindHandler.lua
@@ -2,22 +2,9 @@ local LSU = select(2, ...)
 
 BINDING_HEADER_LIGHTSKY_UNIVERSAL = "Lightsky Universal"
 BINDING_NAME_LSU_MOUNTUP = "Mount Up"
-BINDING_NAME_LSU_ADDJUNKITEM = "Add Junk Item"
 
 function LSUKeybindHandler(key)
     if key == GetBindingKey("LSU_MOUNTUP") then
         LSU.Mount()
-    elseif key == GetBindingKey("LSU_ADDJUNKITEM") then
-        local itemName, itemLink = GameTooltip:GetItem()
-        if itemName and itemLink then
-            local itemID = C_Item.GetItemInfoInstant(itemLink)
-            if itemID then
-                if LSU.IsJunk(itemID) then
-                    LSUDB.Junk[itemID] = nil
-                else
-                    LSUDB.Junk[itemID] = true
-                end
-            end
-        end
     end
 end
