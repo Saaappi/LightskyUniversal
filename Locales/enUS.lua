e4dd766c3b3009a3f2fc451aa0f673fa963983c6
Saaappi <alex.metz1024@gmail.com>
2025-06-15 18:56:13 -0700

    Aligned all the localized strings.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,21 +1,21 @@
 local addonName, LSU = ...
 local L = {}
 
-L.TALENT_IMPORTER_BUTTON_TOOLTIP = "Click to open the talent importer utility."
-L.APPLY_TALENTS_BUTTON_TOOLTIP = "Click to apply talents for your class and specialization."
-L.TEXT_BAD_LOADOUT = "This is a bad loadout! Try importing a new one."
-L.TEXT_CONFIG_IS_NIL = "configID is nil!"
-L.TEXT_TREEID_IS_NIL = "treeID is nil!"
-L.LABEL_TALENT_IMPORTER = "Talent Importer"
-L.LABEL_TALENT_IMPORTER_BUTTON_BACK = "Back"
-L.NEW_CHARACTER_BUTTON_TOOLTIP = "Click to configure new character settings for"
-L.TALENT_IMPORTER_BUTTON_BACK_TOOLTIP = "Return to class selection."
-L.TEXT_BUTTON_CREATION_FAILED = "Button failed to create"
-L.TEXT_CHARACTER_VARIABLE_NIL = "The {character} variable is nil. Please reload."
-L.TEXT_COULD_NOT_RETRIEVE_NPC_INFO = "Could not retrieve NPC information."
-L.TEXT_NO_GOSSIP_OPTIONS_AVAILABLE = "No gossip options available."
-L.TEXT_LAST_UPDATED = "Last Updated"
-L.TEXT_RARE_SPOTTED = "has been spotted"
+L.TALENT_IMPORTER_BUTTON_TOOLTIP        = "Click to open the talent importer utility."
+L.APPLY_TALENTS_BUTTON_TOOLTIP          = "Click to apply talents for your class and specialization."
+L.TEXT_BAD_LOADOUT                      = "This is a bad loadout! Try importing a new one."
+L.TEXT_CONFIG_IS_NIL                    = "configID is nil!"
+L.TEXT_TREEID_IS_NIL                    = "treeID is nil!"
+L.LABEL_TALENT_IMPORTER                 = "Talent Importer"
+L.LABEL_TALENT_IMPORTER_BUTTON_BACK     = "Back"
+L.NEW_CHARACTER_BUTTON_TOOLTIP          = "Click to configure new character settings for"
+L.TALENT_IMPORTER_BUTTON_BACK_TOOLTIP   = "Return to class selection."
+L.TEXT_BUTTON_CREATION_FAILED           = "Button failed to create"
+L.TEXT_CHARACTER_VARIABLE_NIL           = "The {character} variable is nil. Please reload."
+L.TEXT_COULD_NOT_RETRIEVE_NPC_INFO      = "Could not retrieve NPC information."
+L.TEXT_NO_GOSSIP_OPTIONS_AVAILABLE      = "No gossip options available."
+L.TEXT_LAST_UPDATED                     = "Last Updated"
+L.TEXT_RARE_SPOTTED                     = "has been spotted"
 
 -- Button labels
 L.LABEL_ACCEPT_QUESTS                   = "Accept Quests"
@@ -77,26 +77,26 @@ L.HEADER_GOSSIP_MODULE                  = "Gossip Module"
 L.HEADER_NEW_CHARACTER_MODULE           = "New Character Module"
 
 -- Fontstrings
-L.FONTSTRING_GOSSIP_LINE_COUNT_TEXT = "|cffFFFFFF%s|r line(s)"
+L.FONTSTRING_GOSSIP_LINE_COUNT_TEXT     = "|cffFFFFFF%s|r line(s)"
 
 -- Popups
 L.POPUP_NEWCHARACTER_TEXT = "Character configuration completed. Would you like to reload now?\n\n" ..
 "|cffFF474CNOTE|r: This is recommended to guarantee the settings are saved server side."
 
-L.SLASH_CMD_LSU = "/lsu"
+L.SLASH_CMD_LSU                        = "/lsu"
 
 -- Frame titles
-L.TITLE_ADDON = C_AddOns.GetAddOnMetadata(addonName, "Title")
-L.TITLE_GOSSIPS = "Gossips"
-L.TITLE_SETTINGS = "Settings"
+L.TITLE_ADDON                          = C_AddOns.GetAddOnMetadata(addonName, "Title")
+L.TITLE_GOSSIPS                        = "Gossips"
+L.TITLE_SETTINGS                       = "Settings"
 
 -- Tooltips
-L.TOOLTIP_GOSSIPS_OPEN_BUTTON = "Click to open the Gossips utility."
-L.TOOLTIP_GOSSIPS_SEARCHBOX = "Enter a search query here to filter the above edit box, making it significantly easier to locate what you're looking for.\n\n" ..
+L.TOOLTIP_GOSSIPS_OPEN_BUTTON          = "Click to open the Gossips utility."
+L.TOOLTIP_GOSSIPS_SEARCHBOX            = "Enter a search query here to filter the above edit box, making it significantly easier to locate what you're looking for.\n\n" ..
 "|cffFF474CNOTE|r: Your search query can be no more than 30 characters."
-L.TOOLTIP_GOSSIPS_SUBMIT_BUTTON = "Submit the contents of the edit box for processing. If valid, the addon will add them to your Gossips table and automate their selection going forward.\n\n" ..
+L.TOOLTIP_GOSSIPS_SUBMIT_BUTTON        = "Submit the contents of the edit box for processing. If valid, the addon will add them to your Gossips table and automate their selection going forward.\n\n" ..
 "If you submit gossips that are available in the active Gossip Frame, then they will be selected without the need to close and reopen the frame."
-L.TOOLTIP_GOSSIPS_HELP_BUTTON = "|cffFF474CThere is a rough cap on the number of gossips you can store. The exact number will depend on the format of your gossips, but the range is anywhere from 250-275,000.|r\n\n" ..
+L.TOOLTIP_GOSSIPS_HELP_BUTTON          = "|cffFF474CThere is a rough cap on the number of gossips you can store. The exact number will depend on the format of your gossips, but the range is anywhere from 250-275,000.|r\n\n" ..
 "Entering data requires the ID of the NPC/GameObject, the ID of the desired gossip option, and optionally a list of conditions.\n\n" ..
 "The |cff00CCFF<ID>|r for the NPC or GameObject can be found at the top of the GossipFrame when interacting with the target. It prefixes the name within [].\n\n" ..
 "The |cff00CCFF<GossipOptionID>|r can be found before the text of the option within []. It's the number in the |cffBA45A0mulberry|r color.\n\n" ..
@@ -113,45 +113,45 @@ L.TOOLTIP_GOSSIPS_HELP_BUTTON = "|cffFF474CThere is a rough cap on the number of
 "Daisy chaining conditions together is easily done by separating them with a comma:\n" ..
 "- |cff00CCFF!CT_EXPANSION;10,QUEST_ACTIVE;12345|r: First, the addon will check if the player is not in Legion Chromie Time, and then check if they're on the quest with an ID of 12345.\n\n" ..
 "If any condition fails its check, then the gossip will not be selected. Nothing is returned in chat when a condition fails, so if something isn't working as it should, double check your conditions! :D"
-L.TOOLTIP_ACCEPT_QUESTS = "Streamline your gameplay by automatically accepting most available quests from NPCs."
-L.TOOLTIP_AUTO_REPAIR = "Keep your gear in top shape by auto-repairing at vendors, as long as the repair cost is under 33% of your current gold."
-L.TOOLTIP_AUTO_TRAIN = "Effortlessly learn new skills by automatically training all available spells at trainers—limited to affordable costs (up to 33% of your gold)."
-L.TOOLTIP_BUY_QUEST_ITEMS = "Never forget a quest item again! Purchases required quest items from vendors automatically."
-L.TOOLTIP_CHROMIE_TIME = "Automatically choose and activate your preferred Chromie Time expansion when interacting with them."
-L.TOOLTIP_COMPLETE_QUESTS = "Finish quests with automatic turn-ins and selecting rewards."
-L.TOOLTIP_GOSSIP = "Interact with supported NPCs efficiently by instantly selecting gossip options."
-L.TOOLTIP_NEW_CHARACTER = "Configure and manage special settings for new characters with a dedicated module toggle."
-L.TOOLTIP_PLAYER_TALENTS = "Quickly learn new talents at the click of a button!"
-L.TOOLTIP_QUEST_REWARDS = "Automatically selects the best quest reward—prioritizing item level, sell price, or choosing randomly if needed."
-L.TOOLTIP_RARES = "Control rare spawn notifications—toggle announcements for rare NPCs on or off."
-L.TOOLTIP_SKIP_CINEMATICS = "Enjoy uninterrupted gameplay by skipping in-game cinematics and movies automatically."
-L.TOOLTIP_ALWAYS_COMPARE_ITEMS = "Force displays the comparison tooltip when you hover over other items, allowing you to easily compare attributes and stats."
-L.TOOLTIP_ALWAYS_SHOW_ACTION_BARS = "Keeps action bars visible at all times."
-L.TOOLTIP_ARACHNOPHOBIA_MODE = "Replaces arachnid creature models with crabs to provide a more comfortable experience for players with arachnophobia."
-L.TOOLTIP_AUTO_DISMOUNT_FLYING = "If enabled, automatically dismounts your character when casting spells or abilities while flying."
-L.TOOLTIP_AUTO_INTERACT = "Automatically moves your character into range when interacting with NPCs or objects."
-L.TOOLTIP_AUTO_LOOT_DEFAULT = "Automatically collects loot from defeated enemies or opened containers."
+L.TOOLTIP_ACCEPT_QUESTS                = "Streamline your gameplay by automatically accepting most available quests from NPCs."
+L.TOOLTIP_AUTO_REPAIR                  = "Keep your gear in top shape by auto-repairing at vendors, as long as the repair cost is under 33% of your current gold."
+L.TOOLTIP_AUTO_TRAIN                   = "Effortlessly learn new skills by automatically training all available spells at trainers—limited to affordable costs (up to 33% of your gold)."
+L.TOOLTIP_BUY_QUEST_ITEMS              = "Never forget a quest item again! Purchases required quest items from vendors automatically."
+L.TOOLTIP_CHROMIE_TIME                 = "Automatically choose and activate your preferred Chromie Time expansion when interacting with them."
+L.TOOLTIP_COMPLETE_QUESTS              = "Finish quests with automatic turn-ins and selecting rewards."
+L.TOOLTIP_GOSSIP                       = "Interact with supported NPCs efficiently by instantly selecting gossip options."
+L.TOOLTIP_NEW_CHARACTER                = "Configure and manage special settings for new characters with a dedicated module toggle."
+L.TOOLTIP_PLAYER_TALENTS               = "Quickly learn new talents at the click of a button!"
+L.TOOLTIP_QUEST_REWARDS                = "Automatically selects the best quest reward—prioritizing item level, sell price, or choosing randomly if needed."
+L.TOOLTIP_RARES                        = "Control rare spawn notifications—toggle announcements for rare NPCs on or off."
+L.TOOLTIP_SKIP_CINEMATICS              = "Enjoy uninterrupted gameplay by skipping in-game cinematics and movies automatically."
+L.TOOLTIP_ALWAYS_COMPARE_ITEMS         = "Force displays the comparison tooltip when you hover over other items, allowing you to easily compare attributes and stats."
+L.TOOLTIP_ALWAYS_SHOW_ACTION_BARS      = "Keeps action bars visible at all times."
+L.TOOLTIP_ARACHNOPHOBIA_MODE           = "Replaces arachnid creature models with crabs to provide a more comfortable experience for players with arachnophobia."
+L.TOOLTIP_AUTO_DISMOUNT_FLYING         = "If enabled, automatically dismounts your character when casting spells or abilities while flying."
+L.TOOLTIP_AUTO_INTERACT                = "Automatically moves your character into range when interacting with NPCs or objects."
+L.TOOLTIP_AUTO_LOOT_DEFAULT            = "Automatically collects loot from defeated enemies or opened containers."
 L.TOOLTIP_AUTO_PUSH_SPELLS_TO_ACTION_BAR = "Automatically adds newly learned spells to your action bar when you level up."
-L.TOOLTIP_CLEAR_ALL_TRACKING = "Automatically removes all active tracking options from the minimap."
-L.TOOLTIP_COOLDOWN_VIEWER = "Displays a visual tracker on your screen for ability cooldowns."
+L.TOOLTIP_CLEAR_ALL_TRACKING           = "Automatically removes all active tracking options from the minimap."
+L.TOOLTIP_COOLDOWN_VIEWER              = "Displays a visual tracker on your screen for ability cooldowns."
 L.TOOLTIP_DISABLE_SKYRIDING_FULL_SCREEN_EFFECT = "Disables full screen visual effects while skyriding for a clearer view and reduced visual distractions."
-L.TOOLTIP_DISABLE_SKYRIDING_VFX = "Disables velocity visual effects while skyriding to reduce motion sickness and provide a more comfortable experience."
+L.TOOLTIP_DISABLE_SKYRIDING_VFX        = "Disables velocity visual effects while skyriding to reduce motion sickness and provide a more comfortable experience."
 L.TOOLTIP_DISABLE_USER_ADDONS_BY_DEFAULT = "Prevents newly installed addons from being enabled automatically."
-L.TOOLTIP_ENABLE_FLOATING_COMBAT_TEXT = "Enables or disables floating combat text for damage, healing, and other events during combat."
-L.TOOLTIP_DISABLE_FOOTSTEP_SOUNDS = "Enables or disables footstep sounds."
-L.TOOLTIP_LOOT_UNDER_MOUSE = "Displays the loot window beneath your mouse cursor."
-L.TOOLTIP_MOUNT_JOURNAL_SHOWS_PLAYER = "Automatically displays your character model riding mounts in the Mount Journal preview."
-L.TOOLTIP_OCCLUDED_SILHOUETTE_PLAYER = "Displays your character's silhouette when view is blocked by objects, such as trees, for improved tracking."
-L.TOOLTIP_ENABLE_PROFANITY_FILTER = "Enables the in-game profanity filter to block offensive language in chat."
+L.TOOLTIP_ENABLE_FLOATING_COMBAT_TEXT  = "Enables or disables floating combat text for damage, healing, and other events during combat."
+L.TOOLTIP_DISABLE_FOOTSTEP_SOUNDS      = "Enables or disables footstep sounds."
+L.TOOLTIP_LOOT_UNDER_MOUSE             = "Displays the loot window beneath your mouse cursor."
+L.TOOLTIP_MOUNT_JOURNAL_SHOWS_PLAYER   = "Automatically displays your character model riding mounts in the Mount Journal preview."
+L.TOOLTIP_OCCLUDED_SILHOUETTE_PLAYER   = "Displays your character's silhouette when view is blocked by objects, such as trees, for improved tracking."
+L.TOOLTIP_ENABLE_PROFANITY_FILTER      = "Enables the in-game profanity filter to block offensive language in chat."
 L.TOOLTIP_PVP_FRAMES_DISPLAY_CLASS_COLOR = "Displays player class colors on PvP frames for easier identification of opponents and teammates."
-L.TOOLTIP_QUEST_TEXT_CONTRAST = "Enable to enhance the contrast of the quest giver interface for improved readability and accessibility."
+L.TOOLTIP_QUEST_TEXT_CONTRAST          = "Enable to enhance the contrast of the quest giver interface for improved readability and accessibility."
 L.TOOLTIP_RAID_FRAMES_DISPLAY_CLASS_COLOR = "Displays player class colors on raid frames for easier identification of group members."
-L.TOOLTIP_REPLACE_MY_PLAYER_PORTRAIT = "Replaces your character's portrait with your class icon."
+L.TOOLTIP_REPLACE_MY_PLAYER_PORTRAIT   = "Replaces your character's portrait with your class icon."
 L.TOOLTIP_REPLACE_OTHER_PLAYER_PORTRAITS = "Replaces the portraits of other players with their class icon."
-L.TOOLTIP_SHOW_SCRIPT_ERRORS = "Displays Lua script errors as they occur to help identify and troubleshoot addon or interface issues."
-L.TOOLTIP_SHOW_TARGET_OF_TARGET = "Shows the target of your current target for improved situational awareness."
-L.TOOLTIP_SHOW_TUTORIALS = "Displays tutorial popups to guide you through gameplay and features."
-L.TOOLTIP_SOFT_TARGET_ENEMY = "Highlights or selects enemies you're looking at without requiring a hard target, enabling quick interaction or awareness."
-L.TOOLTIP_SPELLBOOK_HIDE_PASSIVES = "Hides passive spells in your spellbook for a clearer view of active abilities."
+L.TOOLTIP_SHOW_SCRIPT_ERRORS           = "Displays Lua script errors as they occur to help identify and troubleshoot addon or interface issues."
+L.TOOLTIP_SHOW_TARGET_OF_TARGET        = "Shows the target of your current target for improved situational awareness."
+L.TOOLTIP_SHOW_TUTORIALS               = "Displays tutorial popups to guide you through gameplay and features."
+L.TOOLTIP_SOFT_TARGET_ENEMY            = "Highlights or selects enemies you're looking at without requiring a hard target, enabling quick interaction or awareness."
+L.TOOLTIP_SPELLBOOK_HIDE_PASSIVES      = "Hides passive spells in your spellbook for a clearer view of active abilities."
 
 LSU.L = L
