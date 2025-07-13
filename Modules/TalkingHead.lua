a13876acedf7b2d3a19b610f92ed3e7059c82b67
Saaappi <alex.metz1024@gmail.com>
2025-07-13 00:33:21 -0700

    Corrected the setting name in the Talking Head module.

--- Modules/TalkingHead.lua
+++ Modules/TalkingHead.lua
@@ -1,7 +1,7 @@
 local addonTable = select(2, ...)
 
 hooksecurefunc(TalkingHeadFrame, "PlayCurrent", function(self)
-    if not LSUDB.Settings["TalkingHead.Disabled"] then return end
+    if not LSUDB.Settings["MuteTalkingHead.Enabled"] then return end
 
     self:Reset()
     self:CloseImmediately()
