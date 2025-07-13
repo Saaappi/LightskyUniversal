eb91f9c61aa1e4d717645ed6e79c3a02845e5420
Saaappi <alex.metz1024@gmail.com>
2025-07-12 19:30:51 -0700

    No longer registering the GOSSIP_SHOW event.

--- Modules/Gossip.lua
+++ Modules/Gossip.lua
@@ -6,14 +6,9 @@ local formatters = {
 }
 
 eventFrame:RegisterEvent("GOSSIP_CONFIRM")
-eventFrame:RegisterEvent("GOSSIP_SHOW")
 eventFrame:SetScript("OnEvent", function(_, event, ...)
     if event == "GOSSIP_CONFIRM" then
         C_Timer.After(0.1, function() StaticPopup1Button1:Click() end)
-    elseif event == "GOSSIP_SHOW" then
-        C_Timer.After(0.1, function()
-            --addonTable.ProcessQuestsAndGossipsSequentially(addonTable.QuestGossipShowAPI())
-        end)
     end
 end)
 
